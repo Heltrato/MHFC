f2325f198a4213302238e93e779fcff223017105
heltrato <jayrol_rante2006@yahoo.com>
2021-01-26 21:38:51 +0800

    Updatd to 1.16.5

---
+++ src/main/java/com/heltrato/mhfc/entity/goal/BasicGoal.java
@@ -0,0 +1,6 @@
+package com.heltrato.mhfc.entity.goal;
+
+public class BasicGoal {
+
+    // This will store every mutex and a implementation of some future methods that will implies every movement for the entity
+}
