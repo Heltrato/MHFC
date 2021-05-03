d09b6b62824df2af93079f9feef17fa6a24df4b5
heltrato <jayrol_rante2006@yahoo.com>
2021-05-04 01:02:54 +0800

    1.16.5 intially commit

--- src/main/java/com/heltrato/mhfc/entity/goal/BasicGoal.java
+++ src/main/java/com/heltrato/mhfc/entity/goal/BasicGoal.java
@@ -1,6 +1,14 @@
 package com.heltrato.mhfc.entity.goal;
 
-public class BasicGoal {
+import net.minecraft.entity.ai.goal.Goal;
+
+public class BasicGoal extends Goal {
+
+	@Override
+	public boolean shouldExecute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
     // This will store every mutex and a implementation of some future methods that will implies every movement for the entity
 }
